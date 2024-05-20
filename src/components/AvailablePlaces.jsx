0a30629007b0213e14186467121164d2d57b3803
Natalia <sh.nat.vlad@gmail.com>
2024-05-20 15:25:52 +0500

    feat: add error handling try/catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,19 +1,32 @@
 import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [ isFetching, setIsFetching ] = useState(false);
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ error, setError ] = useState()
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+  
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message: error.message || 'Could not fetch places, please try again later'});
+      }
+
       setIsFetching(false);
     }
     
@@ -27,6 +40,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
     //     setAvailablePlaces(resData.places);
     // })
 }, []);
+
+  if (error) {
+    return (
+      <Error title="An error occurred!" message={error.message} />
+    )
+  }
 
   return (
     <Places
