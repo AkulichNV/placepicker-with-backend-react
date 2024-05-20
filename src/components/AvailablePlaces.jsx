44aaeae6df3a1b8f7f4ab457e42076018e3df64f
Natalia <sh.nat.vlad@gmail.com>
2024-05-20 14:57:03 +0500

    feat: add text with fetching place data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,17 +5,20 @@ import Places from './Places.jsx';
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [ isFetching, setIsFetching ] = useState(false);
   const [ availablePlaces, setAvailablePlaces ] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     
     fetchPlaces();
-
+    
     // fetch('http://localhost:3000/places')
     //   .then((response) => {
     //     return response.json();
@@ -29,6 +32,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
